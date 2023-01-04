7ec9367435686ab303228fc64e389c4075d934e6
Camilo-J <josecamilo1902@outlook.com>
2023-01-04 14:02:00 -0500

    Add image component

---
+++ src/components/image.jsx
@@ -0,0 +1,29 @@
+import styled from "@emotion/styled";
+
+const sizeOptions = {
+  sm: "130px",
+};
+
+const Photo = styled.div`
+  width: ${({ size }) => sizeOptions[size]};
+  height: ${sizeOptions.sm};
+  box-shadow: 0px 20px 20px rgba(0, 0, 0, 0.2);
+  border-radius: 100%;
+`;
+
+const Img = styled.img`
+  object-fit: cover;
+  border-radius: 100%;
+  width: 100%;
+  height: 100%;
+`;
+
+const Image = ({ src, size }) => {
+  return (
+    <Photo size={size}>
+      <Img src={src} alt={"food's menu"} />
+    </Photo>
+  );
+};
+
+export default Image;
