414080c50f3242bc1cbf570367a59becabf15edc
Camilo-J <josecamilo1902@outlook.com>
2023-01-03 19:54:05 -0500

    Add reset and global styles

---
+++ src/styles/colors.js
@@ -0,0 +1,22 @@
+export const colors = {
+  black: "#333333",
+  white: "#FFFFFF",
+
+  orange: {
+    500: "#F97316",
+  },
+  teal: {
+    500: "#14B8A6",
+  },
+  cyan: {
+    500: "#06B6D4",
+  },
+  lightBlue: {
+    500: "#0EA5E9",
+  },
+  blue: {
+    500: "#2D9CDB",
+  },
+
+  background: "#F6F6F9",
+};
