50c6ba686cd9d973cd97d7abd9dcc57eeeaf3bc5
Camilo-J <josecamilo1902@outlook.com>
2023-01-05 09:00:09 -0500

    Add Position sticky in navbar & background

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,15 @@ import { RxCounterClockwiseClock } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { FiUser } from "react-icons/fi";
 import NavBarItem from "./navbarItem";
+import { colors } from "../styles";
 
 const NavbarC = styled("div")`
+  background-color: ${colors.background};
   display: flex;
   justify-content: space-around;
-  padding-bottom: 20px;
+  padding: 10px 0;
+  position: sticky;
+  bottom: 0px;
 `;
 
 function Navbar() {
