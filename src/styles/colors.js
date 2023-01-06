44745eb041d5676889a72afd27288c0120cc6e3d
Camilo-J <josecamilo1902@outlook.com>
2023-01-06 14:47:12 -0500

    Delete colors

--- src/styles/colors.js
+++ src/styles/colors.js
@@ -5,18 +5,6 @@ export const colors = {
   orange: {
     500: "#F97316",
   },
-  teal: {
-    500: "#14B8A6",
-  },
-  cyan: {
-    500: "#06B6D4",
-  },
-  lightBlue: {
-    500: "#0EA5E9",
-  },
-  blue: {
-    500: "#2D9CDB",
-  },
 
   background: "#F6F6F9",
 };
