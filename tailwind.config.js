46449b1fa19076372b12bab644798a8c716c70b2
Camilo-J <josecamilo1902@outlook.com>
2024-12-05 20:29:58 -0500

    chore: add tempo library to handle dates

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        'code': ['Source Code Pro', 'monospace', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI',
+          'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Open Sans', 'Helvetica Neue', 'sans-serif']
+      }
+    }
   },
   plugins: []
 };
