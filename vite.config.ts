93685ee845ae73d8f781ae000b3e84ceb56aedb1
Camilo-J <josecamilo1902@outlook.com>
2024-12-11 18:53:28 -0500

    chore: add tests dependencies and set the setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
-});
+export default defineConfig(() => ({
+  plugins: [react()],
+  resolve: { alias: { '@': resolve(__dirname, 'src') } },
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['./src/__test__/setup.ts']
+  }
+}));
